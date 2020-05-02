bf9fadd3328c8a348e3067a20d66bdbf5527e7fc
Vladislav Kachan <vladislav.kachan@reedonline.co.uk>
2020-05-02 15:01:52 +0300

    Project

---
+++ Observable/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  Observable
+//
+//  Created by Uladzislau Kachan on 1/6/20.
+//  Copyright © 2020 Uladzislau Kachan. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
---
+++ Observable/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Observable
+//
+//  Created by Uladzislau Kachan on 1/6/20.
+//  Copyright © 2020 Uladzislau Kachan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
