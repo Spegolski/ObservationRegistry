31bc7d3af8470e710053049910016abfaeb666e5
Vladislav Kachan <vladislav.kachan@reedonline.co.uk>
2020-05-02 15:03:12 +0300

    Observation registry

---
+++ ObservationRegistry/Observable.swift
@@ -0,0 +1,55 @@
+//
+//  Observable.swift
+//  Observable
+//
+//  Created by Uladzislau Kachan on 5/2/20.
+//  Copyright © 2020 Uladzislau Kachan. All rights reserved.
+//
+
+import Foundation
+
+protocol Convertible: Hashable {
+    var identifier: Int { get }
+}
+
+extension Convertible {
+    var identifier: Int {
+        return self.hashValue
+    }
+}
+
+protocol Observable {
+    associatedtype Event where Event: Convertible
+
+    func notify(event: Event)
+}
+
+extension Observable {
+    func notify(event: Event) {
+        Registry.shared.fire(event: event.identifier)
+    }
+}
+
+class FirstObservable: Observable {
+    enum Values: Convertible {
+        case didChangeValue
+    }
+    
+    typealias Event = Values
+    
+    func didChangeValue() {
+        self.notify(event: .didChangeValue)
+    }
+}
+
+class SecondObservable: Observable {
+    enum Values: Convertible {
+        case didTapButton
+    }
+    
+    typealias Event = Values
+    
+    func didChangeValue() {
+        self.notify(event: .didTapButton)
+    }
+}
